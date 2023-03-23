8a7b905d58f8a8379e88eb1686989a1b8a697d7c
Arek <arexspeed95@gmail.com>
2023-03-23 13:50:05 +0100

    styling links

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,120 +1,196 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { HomeIcon } from "../Icons";
+import {
+  AuctionIcon,
+  HeartIcon,
+  HomeIcon,
+  OrdersIcon,
+  PremiumIcon,
+  ProductsIcon,
+  SalesIcon,
+  TicketIcon,
+} from "../Icons";
 
-const iconsStyle = "ml-2 w-[24px] h-[24px] text-blue";
 const itemStyle = "flex w-full justify-center";
-const linkStyle =
-  "mt-2.5  flex items-center w-[213px] h-[40px] rounded-lg hover:bg-lightblue";
-const linkActiveStyle =
-  "mt-2.5 flex items-center w-[213px] h-[40px] bg-lightblue rounded-lg";
-const spanStyle = "ml-2.5 text-md text-blue";
+const linkStyle = "flex items-center px-2 py-1 w-full rounded-lg hover:bg-blue";
+const linkActiveStyle = "flex items-center px-2 py-1 w-full rounded-lg bg-blue";
+const spanStyle = "pl-2 font-semibold text-md text-grayDark";
+const spanActiveStyle = "pl-2 font-semibold text-md text-white";
 
 export const Sidebar = () => {
   const router = useRouter();
   return (
     <nav className="flex bg-primary justify-between flex-col w-[256px] sticky bg-white">
-      <ul>
+      <ul className="flex flex-col gap-2 px-4">
         <li className={itemStyle}>
-          <Link href="/" passHref>
+          <Link
+            href="/"
+            passHref
+            className={
+              router.pathname === "/" ? `${linkActiveStyle}` : `${linkStyle}`
+            }
+          >
             <HomeIcon />
             <span
               className={
-                router.pathname === "/" ? `${linkActiveStyle}` : `${linkStyle}`
+                router.pathname === "/" ? `${spanActiveStyle}` : `${spanStyle}`
               }
             >
-              <span className="ml-2.5 text-md text-grayDark">Home</span>
+              Home
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/products" passHref>
+          <Link
+            href="/products"
+            passHref
+            className={
+              router.pathname === "/products"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <ProductsIcon />
             <span
               className={
                 router.pathname === "/products"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className="ml-2.5 text-md text-blue">Products</span>
+              Products
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/sales" passHref>
+          <Link
+            href="/sales"
+            passHref
+            className={
+              router.pathname === "/sales"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <SalesIcon />
             <span
               className={
                 router.pathname === "/sales"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className="ml-2.5 text-md text-blue-600">Sales</span>
+              Sales
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/orders" passHref>
+          <Link
+            href="/orders"
+            passHref
+            className={
+              router.pathname === "/orders"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <OrdersIcon />
             <span
               className={
                 router.pathname === "/orders"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className={spanStyle}>Orders</span>
+              Orders
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/auctions" passHref>
+          <Link
+            href="/auctions"
+            passHref
+            className={
+              router.pathname === "/auctions"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <AuctionIcon />
             <span
               className={
                 router.pathname === "/auctions"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className={spanStyle}>Auctions</span>
+              Auctions
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/tickets" passHref>
+          <Link
+            href="/tickets"
+            passHref
+            className={
+              router.pathname === "/tickets"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <TicketIcon />
             <span
               className={
                 router.pathname === "/tickets"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className={spanStyle}>Tickets</span>
+              Tickets
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/premium" passHref>
+          <Link
+            href="/premium"
+            passHref
+            className={
+              router.pathname === "/premium"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <PremiumIcon />
             <span
               className={
                 router.pathname === "/premium"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className={spanStyle}>Premium</span>
+              Premium
             </span>
           </Link>
         </li>
         <li className={itemStyle}>
-          <Link href="/favorite" passHref>
+          <Link
+            href="/favorite"
+            passHref
+            className={
+              router.pathname === "/favorite"
+                ? `${linkActiveStyle}`
+                : `${linkStyle}`
+            }
+          >
+            <HeartIcon />
             <span
               className={
                 router.pathname === "/favorite"
-                  ? `${linkActiveStyle}`
-                  : `${linkStyle}`
+                  ? `${spanActiveStyle}`
+                  : `${spanStyle}`
               }
             >
-              <span className={spanStyle}>Favorite</span>
+              Favorite
             </span>
           </Link>
         </li>
