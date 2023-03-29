42854148aaa2f04c82d00cd2b9fbc64f0580d4bd
Kordian <kordian.pilarski@capgemini.com>
2023-03-29 14:52:04 +0200

    add Head component in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,6 +42,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <MenuProvider>
+        <Head>
+          <title>
+            {router.route !== "/" ? titleString() : ""}
+            Club Sport Shop
+          </title>
+          <meta
+            name="description"
+            content="Club Sport Shop - where you can find your sports clothes"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <main
           className={`${inter.variable} ${russo.variable} font-sans max-w-[1920px] mx-auto`}
         >
