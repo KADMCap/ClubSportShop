943db3ec4c0b73f8a2c3693128b1fa9b869ba117
Arek <arexspeed95@gmail.com>
2023-03-30 10:18:31 +0200

    move infinity to orders

--- pages/sales.tsx
+++ pages/sales.tsx
@@ -1,7 +1,8 @@
 import { Layout } from "@/components/Layout";
 import Link from "next/link";
-import React from "react";
-import { useQuery } from "react-query";
+import React, { useMemo, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useQuery, useInfiniteQuery } from "react-query";
 
 interface StoreApiResponse {
   id: number;
@@ -25,40 +26,93 @@ const getProducts = async () => {
 };
 
 export default function SalesPage() {
-  const result = useQuery("products", getProducts);
+  //const result = useQuery("products", getProducts);
+  const [take, setTake] = useState(20);
 
-  if (result.isLoading) {
-    return <p>Loading...</p>;
-  }
+  const { data, error, fetchNextPage, status, hasNextPage } = useInfiniteQuery(
+    ["products"],
+    ({ pageParam }) =>
+      fetch(
+        `https://naszsklep-api.vercel.app/api/products?take=${take}&offset=0`
+      ).then((res) => res.json()),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        console.log({ lastPage, allPages });
+        const previousPage = lastPage.length + 20;
+        setTake(previousPage);
 
-  if (!result.data || result.error) {
-    return <p>Something went wrong</p>;
-  }
+        //const currentPage = previousPage + 25;
+
+        // if (currentPage === lastPage.info.pages) return false;
+        //return currentPage + 25;
+        // const previousPage = lastPage.info.prev
+        //   ? +lastPage.info.prev.split("=")[1]
+        //   : 0;
+        // const currentPage = previousPage + 1;
+
+        // if (currentPage === lastPage.info.pages) return false;
+        // return currentPage + 1;
+      },
+    }
+  );
+  //fetch(`https://rickandmortyapi.com/api/character/?page=${pageParam}`)
+  // console.log({ data });
+
+  const products = useMemo(
+    () =>
+      data?.pages.reduce((prev, page) => {
+        return {
+          //info: page.info,
+          results: [...prev.results, ...page.results],
+        };
+      }),
+    [data]
+  );
+  //console.log({ products });
+
+  if (status === "loading") return <p>Loading...</p>;
+
+  if (status === "error") return <h4>Ups!, {`${error}` as string}</h4>;
+
+  // if (result.isLoading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (!result.data || result.error) {
+  //   return <p>Something went wrong</p>;
+  // }
   return (
     <>
       <Layout>
-        <div className="flex flex-col">
+        <div className="flex flex-col p-4">
           <div className="text-xl">Sales Page</div>
-          <div className="grid grid-cols-4 gap-2">
-            {result.data.map((product) => {
-              return (
-                <Link
-                  href={`/products/${product.id}`}
-                  key={product.id}
-                  className="flex flex-col bg-white rounded-lg"
-                >
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="max-h-[150px] object-contain"
-                  />
-                  <h3 className="font-semibold">{product.title}</h3>
-                  <p>{product.description.substring(0, 150)}</p>
-                  <p>{product.price}</p>
-                </Link>
-              );
-            })}
-          </div>
+          <InfiniteScroll
+            dataLength={products ? products.length : 0}
+            next={() => fetchNextPage()}
+            hasMore={!!hasNextPage}
+            loader={<p>Loading...</p>}
+          >
+            <div className="grid grid-cols-4 gap-2">
+              {products?.results?.map((product: any) => {
+                return (
+                  <Link
+                    href={`/products/${product.id}`}
+                    key={product.id}
+                    className="flex flex-col bg-white rounded-lg"
+                  >
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="max-h-[150px] object-contain"
+                    />
+                    <h3 className="font-semibold">{product.title}</h3>
+                    {/* <p>{product.description.substring(0, 150)}</p> */}
+                    {/* <p>{product.price}</p> */}
+                  </Link>
+                );
+              })}
+            </div>
+          </InfiniteScroll>
         </div>
       </Layout>
     </>
