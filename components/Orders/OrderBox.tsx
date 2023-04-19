43b89365eec875d388ca66f6266761718b2fd809
Arek <arexspeed95@gmail.com>
2023-04-19 13:17:27 +0200

    order box view on page

--- components/Orders/OrderBox.tsx
+++ components/Orders/OrderBox.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { OrderItem } from "./OrderItem";
+import { ChevronDownIcon } from "../Icons";
 
 export const OrderBox = () => {
-  return <div>OrderBox</div>;
+  return (
+    <div className="flex flex-col w-full h-auto gap-4 px-4 py-2 rounded-md bg-primaryLight dark:bg-primaryDark md:rounded-lg">
+      <section className="flex flex-col items-start justify-between md:flex-row md:items-center">
+        <div className="flex flex-row items-center justify-between flex-1 w-full">
+          <p className="font-semibold">
+            Order #10009929{" "}
+            <span className="text-sm text-primaryGray"> / 05.04.2023</span>
+          </p>
+          <button className="bg-transparent outline-none md:hidden">
+            <ChevronDownIcon />
+          </button>
+        </div>
+        <div className="flex flex-row gap-2">
+          <p>
+            Items: <span className="font-semibold">3</span>
+          </p>
+          <p>
+            Total Price: <span className="font-semibold">$174.00</span>
+          </p>
+          <div className="px-1 font-semibold rounded bg-lightGreen">
+            Delivered
+          </div>
+          <button className="hidden bg-transparent outline-none md:flex">
+            <ChevronDownIcon />
+          </button>
+        </div>
+      </section>
+      <section className="flex flex-col gap-1">
+        <OrderItem
+          index={1}
+          imageSrc=""
+          alt="image"
+          title="Super Kit"
+          size="M"
+          productId="1"
+          price="99.99"
+          count={2}
+        />
+        <OrderItem
+          index={2}
+          imageSrc=""
+          alt="image"
+          title="Super Kit"
+          size="M"
+          productId="1"
+          price="99.99"
+          count={2}
+        />
+      </section>
+    </div>
+  );
 };
--- components/Orders/OrderItem.tsx
+++ components/Orders/OrderItem.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import Image from "next/image";
 
-export const OrderItem = () => {
-  return <div>OrderItem</div>;
+interface Props {
+  imageSrc: string;
+  alt: string;
+  title: string;
+  size: string;
+  index: number;
+  productId: string;
+  price: string;
+  count: number;
+}
+
+export const OrderItem = ({
+  imageSrc,
+  alt,
+  title,
+  size,
+  index,
+  productId,
+  price,
+  count,
+}: Props) => {
+  return (
+    <div className="flex flex-row items-center justify-between w-full">
+      <div className="flex items-center justify-center w-5 pr-1 text-sm text-darkGray dark:text-primaryGray">
+        {index}
+      </div>
+      <div className="flex flex-col items-start justify-between w-full p-2 rounded-lg md:items-center md:flex-row bg-secondaryLight dark:bg-secondaryDark dark:text-white">
+        <div className="flex flex-row items-center gap-2">
+          <Image src={imageSrc} alt={alt} width={40} height={40} />
+          <p className="font-semibold">{title}</p>
+        </div>
+        <div className="flex flex-row gap-2">
+          <p>
+            Items: <span className="font-semibold">3</span>
+          </p>
+          <p>
+            Size: <span className="font-semibold">M</span>
+          </p>
+          <p>
+            Price: <span className="font-semibold">$174.00</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
