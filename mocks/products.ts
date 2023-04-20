f56f91464c087423dd3ece988420fdec39432bce
Arek <arexspeed95@gmail.com>
2023-04-20 12:37:37 +0200

    add some mocks data and types

---
+++ mocks/products.ts
@@ -0,0 +1,39 @@
+export interface Product {
+  id: string;
+  title: string;
+  slug: string;
+  promo: boolean;
+  prices: Price[];
+  description: string;
+  category: Category;
+  sport: string;
+  tags: string[];
+  sizes: string[];
+  rating: Rating;
+  images: Image[];
+  bought: number;
+  views: number;
+}
+
+export interface Image {
+  url: string;
+  alt: string;
+}
+
+export interface Price {
+  date: string;
+  price: number;
+}
+
+export interface Rating {
+  rate: number;
+  count: number;
+}
+
+export type Category = "Shirts" | "Shorts" | "Shoes" | "Other";
+export type Sport =
+  | "Football"
+  | "Basketball"
+  | "Volleyball"
+  | "Tennis"
+  | "Running";
