abc765eeb08736a9a69ea1e69dbea3c75de86b6c
Arek <arexspeed95@gmail.com>
2023-04-26 12:22:24 +0200

    add get sales product

--- pages/sales.tsx
+++ pages/sales.tsx
@@ -1,76 +1,84 @@
-import { Button } from "@/components/Buttons/Button";
 import { Layout } from "@/components/Layout";
-import Link from "next/link";
-import React, { useMemo } from "react";
-import { useInfiniteQuery } from "react-query";
+import ProductCard from "@/components/ProductCard";
+import { useQuery, gql } from "@apollo/client";
 
 export default function SalesPage() {
-  const { data, error, fetchNextPage, status, hasNextPage } = useInfiniteQuery(
-    ["products"],
-    ({ pageParam = 20 }) =>
-      fetch(
-        `https://naszsklep-api.vercel.app/api/products?take=${pageParam}&offset=0`
-      ).then((res) => res.json()),
-    {
-      getNextPageParam: (lastPage, allPages) => {
-        console.log({ lastPage, allPages });
-        const previousPage: number = lastPage.length + 20;
-        if (previousPage > 99) return false;
-        return previousPage;
-      },
+  const { loading, error, data } = useQuery(gql`
+    query GetAllProducts {
+      products(first: 24, where: { sale: true }) {
+        createdAt
+        id
+        sale
+        slug
+        title
+        description
+        sport
+        category
+        tags
+        sizes
+        prices {
+          id
+          price
+          date
+        }
+        images {
+          image {
+            id
+            url
+          }
+          alt
+        }
+        rating
+      }
     }
-  );
-  console.log({ data });
-
-  const products = useMemo(
-    () =>
-      data?.pages.reduce((prev, page) => {
-        console.log({ prev, page });
-        return [...page];
-      }),
-    [data]
-  );
-  console.log({ products });
-
-  if (status === "loading") return <p>Loading...</p>;
-
-  if (status === "error") return <h4>Ups!, {`${error}` as string}</h4>;
-
-  return (
-    <>
+  `);
+  if (loading) {
+    return (
       <Layout>
-        <div className="flex flex-col p-4">
+        <div className="flex flex-col">
           <div className="text-xl">Sales Page</div>
-
-          <div className="grid grid-cols-4 gap-2">
-            {products?.map((product: any) => {
-              return (
-                <Link
-                  href={`/products/slug/${product.title}`}
-                  key={product.id}
-                  className="flex flex-col bg-white rounded-lg"
-                >
-                  <img
-                    src={product.image}
-                    alt={product.title}
-                    className="max-h-[150px] object-contain"
-                  />
-                  <h3 className="font-semibold">{product.title}</h3>
-                  {/* <p>{product.description.substring(0, 150)}</p> */}
-                  {/* <p>{product.price}</p> */}
-                </Link>
-              );
-            })}
-          </div>
-          <div className="flex justify-center p-4">
-            {hasNextPage ? (
-              <Button onClick={() => fetchNextPage()}>Load More</Button>
-            ) : (
-              <p>You got all products</p>
-            )}
+          <div className="grid grid-cols-2 gap-2 pb-4 lg:grid-cols-3 xl:grid-cols-4">
+            <p>Loading...</p>
           </div>
         </div>
       </Layout>
-    </>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <div className="flex flex-col">
+          <div className="text-xl">Sales Page</div>
+          <div className="grid grid-cols-2 gap-2 pb-4 lg:grid-cols-3 xl:grid-cols-4">
+            <p>{JSON.stringify(error)}</p>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <div className="flex flex-col">
+        <div className="text-xl">Sales Page</div>
+        <div className="grid grid-cols-2 gap-2 pb-4 lg:grid-cols-3 xl:grid-cols-4">
+          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+          {data.products.map((product: any) => {
+            return (
+              <ProductCard
+                key={product.id}
+                id={product.id.toString()}
+                title={product.title}
+                image={product.images[0].image?.url}
+                prices={product.prices}
+                sale={product.sale}
+                sizes={product.sizes}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </Layout>
   );
 }
