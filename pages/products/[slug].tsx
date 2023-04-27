d696f7c6dac2fec7385178e3f8e06a8ca5dbe187
Arek <arexspeed95@gmail.com>
2023-04-27 10:17:38 +0200

    create get product by slug in graphql

---
+++ pages/products/[slug].tsx
@@ -0,0 +1,100 @@
+import { apolloClient } from "@/graphql/apolloClient";
+import { gql } from "@apollo/client";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import React from "react";
+
+interface GetProductsSlugsResponse {
+  products: Product[];
+}
+
+interface Product {
+  slug: string;
+}
+
+export type InferGetStaticPathsType<T> = T extends () => Promise<{
+  paths: Array<{ params: infer R }>;
+}>
+  ? R
+  : never;
+
+export async function getStaticPaths() {
+  const { data } = await apolloClient.query<GetProductsSlugsResponse>({
+    query: gql`
+      query GetProductsSlugs {
+        products {
+          slug
+        }
+      }
+    `,
+  });
+
+  return {
+    paths: data.products.map((product) => {
+      return {
+        params: {
+          slug: product.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
+
+export const getStaticProps = async ({
+  params,
+}: GetStaticPropsContext<InferGetStaticPathsType<typeof getStaticPaths>>) => {
+  if (!params?.slug) {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
+  const { data } = await apolloClient.query<GetProductsSlugsResponse>({
+    variables: {
+      slug: params.slug,
+    },
+    query: gql`
+      query GetProductDetailBySlug($slug: String) {
+        products(where: { slug: $slug }) {
+          id
+          sale
+          slug
+          title
+          description
+          sport
+          category
+          tags
+          sizes
+          prices {
+            id
+            price
+            date
+          }
+          rating
+          images {
+            image {
+              id
+              url
+            }
+            alt
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const ProductPage = ({
+  data,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log({ data });
+  return <div>ProductPage</div>;
+};
+
+export default ProductPage;
