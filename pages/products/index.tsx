96f88130de8c5004adeb77f140f3b05fe2378aef
Arek <arexspeed95@gmail.com>
2023-04-28 10:56:53 +0200

    get products from query and paginate

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -3,80 +3,109 @@ import { Layout } from "@/components/Layout";
 import ProductCard from "@/components/ProductCard";
 import { InferGetStaticPropsType } from "next";
 import Link from "next/link";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import ReactPaginate from "react-paginate";
 import { FiltersContainer } from "@/components/Filters/FiltersContainer";
+import { gql, useQuery } from "@apollo/client";
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://naszsklep-api.vercel.app/api/products?take=100&offset=0`
-  );
-  const data: StoreApiResponse[] = await res.json();
-  //console.log({ data });
+const query = gql`
+  query GetAllProducts($first: Int, $skip: Int) {
+    products(first: $first, skip: $skip) {
+      createdAt
+      id
+      sale
+      slug
+      title
+      description
+      sport
+      category
+      tags
+      sizes
+      prices {
+        id
+        price
+        date
+      }
+      images {
+        image {
+          id
+          url
+        }
+        alt
+      }
+      rating
+    }
+    productsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
 
-  return {
-    props: {
-      data,
-    },
-  };
-};
-
-export default function ProductsPage({
-  data,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function ProductsPage() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [itemsPerPage, setItemsPerPage] = useState(24);
-  const [itemOffset, setItemOffset] = useState(0);
-  const [endOffset, setEndOffset] = useState(itemOffset + itemsPerPage);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const { loading, error, data } = useQuery(query, {
+    variables: {
+      first: 24,
+      skip: (currentPage - 1) * 24,
+    },
+  });
+  const items = data?.productsConnection.aggregate.count;
 
   const handlePageClick = (event: any) => {
-    console.log({ event });
-    const newOffset = (event.selected * itemsPerPage) % data.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
+    // console.log({ event });
+    // const newOffset = (event.selected * itemsPerPage) % data.length;
+    // console.log(
+    //   `User requested page number ${event.selected}, which is offset ${newOffset}`
+    // );
+
+    window.scrollTo(0, 0);
+    scrollContainer();
     setCurrentPage(event.selected + 1);
-    setItemOffset(newOffset);
+    //setItemOffset(newOffset);
   };
-  useEffect(() => {
-    setEndOffset(+itemOffset + +itemsPerPage);
-    //console.log(itemsPerPage, itemOffset, endOffset);
-  }, [itemsPerPage, itemOffset]);
 
-  const products = useMemo(() => {
-    return data.slice(itemOffset, endOffset);
-  }, [endOffset, itemOffset]);
-
-  console.log("outside", itemsPerPage, itemOffset, endOffset);
+  const scrollContainer = () => {
+    containerRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+  if (loading) {
+    return (
+      <div className="flex flex-col w-full">
+        <FiltersContainer />
+        <p>All products: {items}</p>
+        <div className="grid grid-cols-2 gap-2 pb-4 lg:grid-cols-3 xl:grid-cols-4">
+          <p>Loading...</p>;
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Layout>
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full" ref={containerRef}>
           <FiltersContainer />
+          <p>All products: {items}</p>
           <div className="grid grid-cols-2 gap-2 pb-4 lg:grid-cols-3 xl:grid-cols-4">
-            {products.map((product, index) => {
-              return (
-                <ProductCard
-                  key={product.id}
-                  id={product.id.toString()}
-                  title={product.title}
-                  slug="slug"
-                  image={product.image}
-                  prices={[
-                    {
-                      price: product.price,
-                      date: "2023-04-24",
-                    },
-                  ]}
-                  sale={false}
-                  sizes={["S", "M"]}
-                />
-              );
-            })}
+            {data?.products.map((product: any) => (
+              <ProductCard
+                key={product.id}
+                id={product.id.toString()}
+                title={product.title}
+                slug={product.slug}
+                image={product.images[0].image?.url}
+                prices={product.prices}
+                sale={product.sale}
+                sizes={product.sizes}
+              />
+            ))}
           </div>
-          {currentPage === 1 && (
+
+          {/* {currentPage === 1 && (
             <div className="flex items-center justify-center p-4">
               <p>Products on page:</p>
               <select
@@ -88,7 +117,7 @@ export default function ProductsPage({
                 <option>36</option>
               </select>
             </div>
-          )}
+          )} */}
 
           <div className="flex items-center justify-center p-4">
             <ReactPaginate
@@ -99,7 +128,7 @@ export default function ProductsPage({
               activeLinkClassName="px-2 py-1 text-sm rounded-[4px] bg-primaryBlue dark:bg-darkBlue"
               onPageChange={handlePageClick}
               pageRangeDisplayed={3}
-              pageCount={Math.ceil(data.length / itemsPerPage)}
+              pageCount={Math.ceil(items / 24)}
               previousLabel={<ChevronLeftIcon />}
               //renderOnZeroPageCount={null}
             />
